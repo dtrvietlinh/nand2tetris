84e2decc356d68a0622c38af80d8d2261487691c
VietLinh <dtrvietlinh@gmail.com>
2023-08-23 22:58:06 +0700

    add XML class

---
+++ Compiler/src/main/java/analyzer/grammar/XML.java
@@ -0,0 +1,7 @@
+package analyzer.grammar;
+
+public final class XML {
+	public final static String KEYWORD = "<keyword>";
+	public final static String SYMBOL = "<symbol>";
+	public final static String IDENTIFIER = "<identifier>";
+}
