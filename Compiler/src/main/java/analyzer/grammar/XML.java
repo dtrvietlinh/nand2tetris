c52a061dcc7e97ebb547969d2cd9f432576662d0
VietLinh <dtrvietlinh@gmail.com>
2023-08-26 15:37:06 +0700

    add XML class

--- Compiler/src/main/java/analyzer/grammar/XML.java
+++ Compiler/src/main/java/analyzer/grammar/XML.java
@@ -4,4 +4,7 @@ public final class XML {
 	public final static String KEYWORD = "<keyword>";
 	public final static String SYMBOL = "<symbol>";
 	public final static String IDENTIFIER = "<identifier>";
+	public final static String INT_CONST = "<integerConstant>";
+	public final static String STRING_CONST = "<stringConstant>";
+
 }
